39af4cd37d59870416be8e703ac75234c7847560
Administrator <Administrator@XPD9AZSH6SZAPCX>
2017-06-19 14:15:04 +0800

    护照信息文件添加

---
+++ web/src/main/webapp/res/js/personal/passportInfo/passportInfoList.js
@@ -0,0 +1,8 @@
+$(function(){
+	$("#sex").kendoDropDownList();//性别 状态 下拉框初始化
+	$("#birthDate").kendoDatePicker({culture:"zh-CN",format:"yyyy-MM-dd"});//出生日期
+	$("#signedDate").kendoDatePicker({culture:"zh-CN",format:"yyyy-MM-dd"});//签发日期
+	
+	
+});
+	 
