234427caf8935605744ba93b7d185a4359dc4f5d
崔建斌 <Richardcjb@163.com>
2017-06-22 14:45:49 +0800

    基本信息功能的实现

---
+++ visa/src/main/java/io/znz/jsite/visa/entity/taxpayerauthenticationcode/TaxpayerAuthenticationCodeEntity.java
@@ -0,0 +1,60 @@
+/**
+ * TaxpayerAuthenticationCodeEntity.java
+ * io.znz.jsite.visa.entity.taxpayerauthenticationcode
+ * Copyright (c) 2017, 北京科技有限公司版权所有.
+*/
+
+package io.znz.jsite.visa.entity.taxpayerauthenticationcode;
+
+import java.util.Date;
+
+import lombok.Data;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Comment;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+/**
+ * 美国纳税人认证码
+ * @author   崔建斌
+ * @Date	 2017年6月22日 	 
+ */
+@Data
+@Table("visa_new_taxpayer_authentication_code")
+public class TaxpayerAuthenticationCodeEntity {
+	@Id(auto = true)
+	@Column
+	@Comment("主键")
+	private long id;
+	@Column
+	@Comment("客户id")
+	private long customerId;
+	@Column
+	@Comment("家庭地址")
+	private String homeAddress;
+	@Column
+	@Comment("国家")
+	private String country;
+	@Column
+	@Comment("省份")
+	private String province;
+	@Column
+	@Comment("城市")
+	private String city;
+	@Column
+	@Comment("邮编")
+	private String postCode;
+	@Column
+	@Comment("创建时间")
+	private Date createTime;
+	@Column
+	@Comment("更新时间")
+	private Date updateTime;
+	@Column
+	@Comment("状态")
+	private Integer status;
+	@Column
+	@Comment("备注")
+	private String remark;
+}
