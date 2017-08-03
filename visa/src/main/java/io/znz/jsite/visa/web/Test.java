86bb53418c6b6f560a322416900c07eb169f4087
孙斌 <270361029@qq.com>
2017-08-03 16:32:24 +0800

    后台验证工具类的完成

--- visa/src/main/java/io/znz/jsite/visa/web/Test.java
+++ visa/src/main/java/io/znz/jsite/visa/web/Test.java
@@ -6,9 +6,8 @@
 
 package io.znz.jsite.visa.web;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
+import io.znz.jsite.visa.entity.japan.NewComeBabyJpEntity;
+import io.znz.jsite.visa.zidingyienum.GetCommentUtil;
 
 /**
  * TODO(这里用一句话描述这个类的作用)
@@ -24,11 +23,39 @@ public class Test {
 	private static final int SALT_SIZE = 8; //盐长度
 
 	public static void main(String[] args) {
-		List a = Lists.newArrayList();
-		while (true) {
-			Test t = new Test();
-			a.add(t);
-			System.out.println(a.toString());
-		}
+		/*//		List<Field> list = Arrays.asList(NewComeBabyJpEntity.getClass().getDeclaredFields());
+		Field[] fields = NewComeBabyJpEntity.class.getDeclaredFields();
+		for (int i = 0; i < fields.length; i++) {
+			Field field = fields[i];
+			if (field.isAnnotationPresent(GetComment.class)) {//是否使用GetComment注解
+				for (Annotation anno : field.getDeclaredAnnotations()) {//获得所有的注解
+					if (anno.annotationType().equals(GetComment.class)) {//找到自己的注解
+						System.out.println((GetComment) anno);
+						System.out.println(field.getDeclaredAnnotations());
+						System.out.println(field.getName());
+						System.out.println(field.getAnnotation(GetComment.class).name());
+						if(!((GetComment)anno).isCanNull()){//注解的值
+						    if(TestUtil.getFieldValueByName(field.getName(),obj)==null){
+						        throw new RuntimeException("类："+NewComeBabyJpEntity.class+"的属性："+field.getName()+"不能为空，实际的值:"+TestUtil.getFieldValueByName(field.getName(),obj)+",注解：field.getDeclaredAnnotations()");
+						    }
+						}
+						if(!((GetComment)anno).isCanEmpty()){
+						    if(TestUtil.getFieldValueByName(field.getName(),obj).equals("")){
+						        throw new RuntimeException("类："+NewComeBabyJpEntity.class+"的属性："+field.getName()+"不能为空字符串，实际的值:"+TestUtil.getFieldValueByName(field.getName(),obj)+",注解：field.getDeclaredAnnotations()");
+						    }
+						}
+						if(!((GetComment)anno).isCanZero()){
+						    if(TestUtil.getFieldValueByName(field.getName(),obj).toString().equals("0") || TestUtil.getFieldValueByName(field.getName(),obj).toString().equals("0.0")){
+						        throw new RuntimeException("类："+NewComeBabyJpEntity.class+"的属性："+field.getName()+"不能为空字符0，实际的值:"+TestUtil.getFieldValueByName(field.getName(),obj)+",注解：field.getDeclaredAnnotations()");
+						    }
+						}
+					}
+				}
+			}
+
+		}*/
+
+		String name = GetCommentUtil.getComment(new NewComeBabyJpEntity(), "comFullName");
+		System.out.println(name);
 	}
 }
--- visa/src/main/java/io/znz/jsite/visa/zidingyienum/GetComment.java
+++ visa/src/main/java/io/znz/jsite/visa/zidingyienum/GetComment.java
@@ -6,6 +6,13 @@
 
 package io.znz.jsite.visa.zidingyienum;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * TODO(这里用一句话描述这个类的作用)
  * <p>
@@ -14,7 +21,22 @@ package io.znz.jsite.visa.zidingyienum;
  * @author   孙斌
  * @Date	 2017年8月3日 	 
  */
+@Documented
+@Target(ElementType.FIELD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+//项目启动时就加载
 public @interface GetComment {
+	/**
+	 * 字段名称
+	 *
+	 */
+	String name() default "";
 
+	/**
+	 * 错误原因
+	 *
+	 */
+
+	String text() default "";
 }
-
