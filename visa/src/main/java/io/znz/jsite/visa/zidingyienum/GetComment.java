298123f598eb1c6b95dd8812d67672c77739b67b
孙斌 <270361029@qq.com>
2017-08-03 15:08:56 +0800

    111

---
+++ visa/src/main/java/io/znz/jsite/visa/zidingyienum/GetComment.java
@@ -0,0 +1,20 @@
+/**
+ * GetComment.java
+ * io.znz.jsite.visa.zidingyienum
+ * Copyright (c) 2017, 北京科技有限公司版权所有.
+*/
+
+package io.znz.jsite.visa.zidingyienum;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   孙斌
+ * @Date	 2017年8月3日 	 
+ */
+public @interface GetComment {
+
+}
+
